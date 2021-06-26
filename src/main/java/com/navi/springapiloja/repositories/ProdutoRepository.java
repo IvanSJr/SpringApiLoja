3a50683e8114c45ecbe3ea2b95a9b0c56806bad5
IvanSJr <ivan.junior2706@gmail.com>
2021-06-26 20:42:47 -0300

    Criando associação muitos para muitos e criando dominio Produto com seus repository e services.

---
+++ src/main/java/com/navi/springapiloja/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.navi.springapiloja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.navi.springapiloja.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
