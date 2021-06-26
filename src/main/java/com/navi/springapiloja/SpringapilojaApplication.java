c52f4bab6c0f53de59b27e9235fa30d335db011b
IvanSJr <ivan.junior2706@gmail.com>
2021-06-25 22:01:55 -0300

    Iniciando projeto Spring Boot.

---
+++ src/main/java/com/navi/springapiloja/SpringapilojaApplication.java
@@ -0,0 +1,13 @@
+package com.navi.springapiloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapilojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapilojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navi/springapiloja/SpringapilojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.springapiloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapilojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
