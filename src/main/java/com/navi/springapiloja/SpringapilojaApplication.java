c49ea02e7e4a75cbd2d916c2a360115f98e995d6
IvanSJr <ivan.junior2706@gmail.com>
2021-06-26 19:55:27 -0300

    Criando instãnciação automática de categorias.

--- src/main/java/com/navi/springapiloja/SpringapilojaApplication.java
+++ src/main/java/com/navi/springapiloja/SpringapilojaApplication.java
@@ -1,13 +1,31 @@
 package com.navi.springapiloja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringapilojaApplication {
+import com.navi.springapiloja.domain.Categoria;
+import com.navi.springapiloja.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringapilojaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository serviceRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringapilojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		serviceRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
