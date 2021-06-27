daa3b06f0adabdfb2a840dd1e4a5a457da96fa4c
IvanSJr <ivan.junior2706@gmail.com>
2021-06-26 22:13:52 -0300

    Ajustes no endpoint categorias/{id}. Tratamento de exceções e proteção da referência ciclica na serialização do JSON.

--- src/main/java/com/navi/springapiloja/services/CategoriaService.java
+++ src/main/java/com/navi/springapiloja/services/CategoriaService.java
@@ -7,16 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.navi.springapiloja.domain.Categoria;
 import com.navi.springapiloja.repositories.CategoriaRepository;
-
+import com.navi.springapiloja.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
-	private CategoriaRepository repository;
-	
-	public Categoria buscar(Integer id) { 
-		Optional<Categoria> obj = repository.findById(id); 
-		return obj.orElse(null); 
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto de id " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
 }
