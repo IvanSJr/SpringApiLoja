2785c5169bf5e426f8fe06be61dc88fc0a139649
IvanSJr <ivan.junior2706@gmail.com>
2021-07-03 22:37:24 -0300

    Implementando classes de Cliente e endereço e Enum TipoCliente.

---
+++ src/main/java/com/navi/springapiloja/domain/enums/TipoCliente.java
@@ -0,0 +1,37 @@
+package com.navi.springapiloja.domain.enums;
+
+public enum TipoCliente {
+	PESSOAFISICA(1, "Pessoa física"),
+	PESSOAJURIDICA(2, "Pessoa jurídica");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private TipoCliente(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer codigo) {
+		if (codigo == null) {
+			return null;
+		}
+		
+		for (TipoCliente x: TipoCliente.values()) {
+			if (codigo.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + codigo);
+	}
+	
+}
