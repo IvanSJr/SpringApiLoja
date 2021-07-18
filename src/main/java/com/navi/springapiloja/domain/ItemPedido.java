0cf3784a12f3f3a92494db41c9a924c13a3e951f
IvanSJr <ivan.junior2706@gmail.com>
2021-07-18 12:31:38 -0300

    Fazendo os mapeamentos do objeto relacional e testando o seu funcionamento.

--- src/main/java/com/navi/springapiloja/domain/ItemPedido.java
+++ src/main/java/com/navi/springapiloja/domain/ItemPedido.java
@@ -2,29 +2,32 @@ package com.navi.springapiloja.domain;
 
 import java.io.Serializable;
 
-public class ItemPedido implements Serializable{
-	
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
+	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	private Double desconto;
-	
+
 	public ItemPedido() {
-		
 	}
 
-	public ItemPedido(Pedido pedido, Produto produto,  Integer quantidade, Double preco, Double desconto) {
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
+		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
-		this.desconto = desconto;
 	}
-	
+
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
@@ -32,7 +35,39 @@ public class ItemPedido implements Serializable{
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -58,41 +93,4 @@ public class ItemPedido implements Serializable{
 		return true;
 	}
 
-	public ItemPedidoPK getId() {
-		return id;
-	}
-
-	public void setId(ItemPedidoPK id) {
-		this.id = id;
-	}
-
-	public Integer getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(Integer quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public Double getDesconto() {
-		return desconto;
-	}
-
-	public void setDesconto(Double desconto) {
-		this.desconto = desconto;
-	}
-	
-	
-	
-	
-	
-	
 }
