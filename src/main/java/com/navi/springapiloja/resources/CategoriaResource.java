6cb57333164c9cbfe3db50965a2bb21edebbae56
IvanSJr <ivan.junior2706@gmail.com>
2021-07-27 20:13:54 -0300

    Configurando POST de Categorias, sem verificações.

--- src/main/java/com/navi/springapiloja/resources/CategoriaResource.java
+++ src/main/java/com/navi/springapiloja/resources/CategoriaResource.java
@@ -5,17 +5,20 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.navi.springapiloja.domain.Categoria;
 import com.navi.springapiloja.services.CategoriaService;
 
-@RequestMapping(value = "/categorias")
+@RequestMapping(value = "/categorias",
+		  produces = "application/json")
 @RestController
 public class CategoriaResource {
 
@@ -24,16 +27,23 @@ public class CategoriaResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@PostMapping
-	public ResponseEntity<Void> insert(Categoria obj) {
-	obj = service.inserir(obj);
-	URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-		.path("/{id}").buildAndExpand(obj.getId()).toUri();
-	return ResponseEntity.created(uri).build();
-}
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@ModelAttribute Categoria obj, @PathVariable Integer id){
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 
 }
