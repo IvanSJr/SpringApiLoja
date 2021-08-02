b252dcd4bd3e96e8b09c09a7bf70dfefa7c7c8c4
IvanSJr <ivan.junior2706@gmail.com>
2021-08-02 20:43:53 -0300

    Fazendo a configuração para a inserção de um novo cliente com o metodo Post.

--- src/main/java/com/navi/springapiloja/resources/ClienteResource.java
+++ src/main/java/com/navi/springapiloja/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.navi.springapiloja.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.navi.springapiloja.domain.Cliente;
 import com.navi.springapiloja.dto.ClienteDTO;
+import com.navi.springapiloja.dto.ClienteNewDTO;
 import com.navi.springapiloja.services.ClienteService;
 
 @RequestMapping(value = "/clientes")
@@ -40,7 +44,15 @@ public class ClienteResource {
 		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@PutMapping(value="/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/com/navi/springapiloja/services/ClienteService.java
+++ src/main/java/com/navi/springapiloja/services/ClienteService.java
@@ -9,33 +9,49 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.navi.springapiloja.domain.Cidade;
 import com.navi.springapiloja.domain.Cliente;
+import com.navi.springapiloja.domain.Endereco;
+import com.navi.springapiloja.domain.enums.TipoCliente;
 import com.navi.springapiloja.dto.ClienteDTO;
+import com.navi.springapiloja.dto.ClienteNewDTO;
 import com.navi.springapiloja.repositories.ClienteRepository;
+import com.navi.springapiloja.repositories.EnderecoRepository;
 import com.navi.springapiloja.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
+		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto de id " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
-		return repo.save(newObj);
+		return clienteRepository.save(newObj);
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj = clienteRepository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
-			repo.deleteById(id);
+			clienteRepository.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new com.navi.springapiloja.services.exceptions.DataIntegrityViolationException("Não é possível excluir porque há entidades relacionadas");
@@ -43,16 +59,30 @@ public class ClienteService {
 	}
 
 	public List<Cliente> findAll() {
-		return repo.findAll();
+		return clienteRepository.findAll();
 	}
 
 	public Page<Cliente> findPages(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageRequest);
+		return clienteRepository.findAll(pageRequest);
 	}
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogadouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
