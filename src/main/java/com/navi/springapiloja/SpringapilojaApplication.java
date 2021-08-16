0aadc8c7b089ba4fa3d10b9ce39f9bc46b5bb8d7
IvanSJr <ivan.junior2706@gmail.com>
2021-08-16 20:46:13 -0300

    Salvando arquivo teste no AwS3.

--- src/main/java/com/navi/springapiloja/SpringapilojaApplication.java
+++ src/main/java/com/navi/springapiloja/SpringapilojaApplication.java
@@ -1,21 +1,25 @@
 package com.navi.springapiloja;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.navi.springapiloja.services.S3Service;
+
 @SpringBootApplication
 public class SpringapilojaApplication implements CommandLineRunner{
 	
-
+	@Autowired
+	private S3Service service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringapilojaApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
+	public void run(String... args) throws Exception {	
+		service.uploadFile("C:\\Users\\Ferra\\Downloads\\51377009249_eb809a034c_o.jpg");
 	}
 
 }
